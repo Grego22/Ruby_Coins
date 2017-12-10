4f8192aa8ef1c511dcc70e126585c931302c98a4
Greg Garville Jr <gjgarvil@gmail.com>
2017-12-09 20:51:41 -0500

    created client.rb and set up faraday gem/ HTTP get requests

---
+++ stage1/client.rb
@@ -0,0 +1,18 @@
+require_relative 'Gregocoin'
+require 'faraday'
+
+URL = 'http://localhost/'
+PORT = 7777
+
+def create_user(name)
+    Faraday.post("#{URL}:#{PORT}/users", name: name.body)
+end
+
+def get_balance(user)
+    Faraday.get("#{URL}:#{PORT}/balance)", user: user.body
+end
+
+def transfer(from, to, amount)
+    Faraday.post("#{URL}:#{PORT}/transfer", from: from,
+    to: to, amount: amount).body
+end
