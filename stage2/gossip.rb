e39a46e3be62bf127ef8f56aa9f541720b2b8bd5
Greg Garville Jr <gjgarvil@gmail.com>
2017-12-09 23:48:29 -0500

    added update state to change favorite movie and version number method

--- stage2/gossip.rb
+++ stage2/gossip.rb
@@ -12,14 +12,25 @@ update_state(PORT => nil)
 update_state(PEER_PORT => nil)
 
 MOVIES = File.readlines("movies.txt").map(&:chomp)
-@favorite_movie
-@version_number
-puts "My favorite movie, forever and ever is #{favorite_movie.green}"
+@favorite_movie = MOVIES.sample
+@version_number = 0
+puts "My favorite movie, forever and ever is #{favorite_movie.green}!"
 
 update_state(PORT => [@favorite_movie, @version_number])
 
 every(7.seconds) do
+    puts "I changed my mind. 
+    I no longer like #{favorite_movie.yellow} it wasn't as good on multiple watches."
+    @favorite_movie = MOVIES.sample
+    @version_number += 1
+    update_state(PORT => [@favorite_movie, @version_number])
+    puts "My new favroite movie is #{favorite_movie.red}"
 end
 
 every(3.seconds) do
+    render_state
+    
+end
+
+post '/gossip' do
 end
