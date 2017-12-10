3f513b81878aa79fabda7427342e49ef7e625507
Greg Garville Jr <gjgarvil@gmail.com>
2017-12-09 21:24:20 -0500

    fixed ) syntax error, faraday running now

--- stage1/client.rb
+++ stage1/client.rb
@@ -5,11 +5,11 @@ URL = 'http://localhost/'
 PORT = 7777
 
 def create_user(name)
-    Faraday.post("#{URL}:#{PORT}/users", name: name.body)
+    Faraday.post("#{URL}:#{PORT}/users", name: name).body
 end
 
 def get_balance(user)
-    Faraday.get("#{URL}:#{PORT}/balance)", user: user.body
+    Faraday.get("#{URL}:#{PORT}/balance", user: user).body
 end
 
 def transfer(from, to, amount)
